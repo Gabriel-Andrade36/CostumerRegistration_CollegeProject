995ed919459d20d988c54d48448403cb084022e5
Gabriel-Andrade36git config --global user.email gabrielndcorrea@gmail.com <gabrielndcorrea@gmail.com>
2020-10-05 20:36:43 -0300

    Adicionado projeto ao github

---
+++ CadastroClientes/src/cadastroclientes/CadastroClientes.java
@@ -0,0 +1,17 @@
+
+package cadastroclientes;
+
+public class CadastroClientes {
+
+   
+    public static void main(String[] args) {
+   CadastroUI form = new CadastroUI();   // local onde fica o cadastro
+   Pessoa pessoa = new Pessoa(form.name,form.CPF,form.City,form.State,form.Address);    //local onde as informações cadastradas são salvas    
+    
+    //chama cadastro
+    //cadastra pessoa
+   
+    
+    }
+    
+}
